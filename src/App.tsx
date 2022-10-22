5c163124eda42276daf1d9db2254bb7b0676f425
Santiago David <santiagodaviduno@hotmail.com>
2022-10-22 12:53:32 -0400

    +

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
-import './App.css'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Main from './components/Main'
-
+import { useState } from "react";
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import { Routes, Route } from "react-router-dom";
+import useFirebase from "./hooks/useFirebase";
 
 const App = () => {
-  return (
-  <>
-  <Header></Header>
-  <Main></Main>
-  <Footer></Footer>
-  </>)
-}
 
-export default App
+  const { auth } = useFirebase()
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/*" element={<Main />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
--- src/hooks/useFirebase.ts
+++ src/hooks/useFirebase.ts
@@ -1,22 +1,41 @@
+//Firebase Functions
 import { initializeApp } from "firebase/app";
+import {
+  signInWithPopup,
+  signInWithEmailLink,
+  getAuth,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
+//Config
 const firebaseConfig = {
   apiKey: "AIzaSyDvK91OMFdNBWGSW8smxtqxs8mIzi0Y7gs",
   authDomain: "yes-i-do-5eef4.firebaseapp.com",
   projectId: "yes-i-do-5eef4",
   storageBucket: "yes-i-do-5eef4.appspot.com",
   messagingSenderId: "575879530685",
-  appId: "1:575879530685:web:6eacd6d3b5bed30d924fd4"
+  appId: "1:575879530685:web:6eacd6d3b5bed30d924fd4",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Initialize Firebase Functions
 
 //Firebase Common Hook
 const useFirebase = () => {
-    return {
-        app,
-    };
-}
- 
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+  const storage = getStorage(app);
+  const GoogleProvider = new GoogleAuthProvider();
+
+  return {
+    app,
+    auth,
+    db,
+    storage,
+    GoogleProvider,
+  };
+};
+
 export default useFirebase;
